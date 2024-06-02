05b0470682994c4b8ce9e571a471d749eb57a277
trev-org04 <58343522+trev-org04@users.noreply.github.com>
2024-06-01 22:08:50 -0400

    Testing

--- main.js
+++ main.js
@@ -7,9 +7,6 @@ const menuButton = document.getElementById("menu-button");
 const shareHeader = document.getElementById("share-title-header");
 const shareIcons = document.getElementById("share-icons-container");
 
-const allButtons = [moaButton, vrButton, shareButton, fullscreenButton, menuButton];
-const desktopButtons = [fullscreenButton, menuButton];
-
 var isExpanded = true;
 
 // Function to check if the device is a PC or laptop
@@ -24,18 +21,18 @@ if (isPCorLaptop()) {
     vrButton.style.display = 'none';
     shareButton.style.display = 'none';
 }
-// else {
-//     shareHeader.style.display = 'none';
-//     shareIcons.style.display = 'none';
-// }
 
 // document.addEventListener('DOMContentLoaded', function() {
-//     shareButton.addEventListener('click', function() {
-//         navigator.share({
-//             text: 'Check out this virual home tour through Welcome Home!',
-//             url: window.location.href
-//         });
-//     });
+    if (!isPCorLaptop) {
+        shareHeader.style.display = 'none';
+        shareIcons.style.display = 'none';
+        // shareButton.addEventListener('click', function() {
+        //     navigator.share({
+        //         text: 'Check out this virual home tour through Welcome Home!',
+        //         url: window.location.href
+        //     });
+        // });
+    }
 // });
 
 // Function to request motion and orientation permissions
@@ -110,20 +107,10 @@ function hideAllButtons() {
     menuButton.style.fontSize = '0px';
     menuButton.style.opacity = '0';
     menuButton.style.visibility = 'hidden';
-    // allButtons.forEach((button) => {
-    //     button.style.fontSize = '0px';
-    //     button.style.opacity = '0';
-    //     button.style.visibility = 'hidden';
-    // });
 }
 
 function showButtons() {
     if (!isPCorLaptop()) {
-        // allButtons.forEach((button) => {
-        //     button.style.fontSize = '1.25em';
-        //     button.style.opacity = '1';
-        //     button.style.visibility = 'visible';
-        // });
         moaButton.style.fontSize = '1.25em';
         moaButton.style.opacity = '1';
         moaButton.style.visibility = 'visible';
@@ -134,13 +121,6 @@ function showButtons() {
         shareButton.style.opacity = '1';
         shareButton.style.visibility = 'visible';
     } 
-    // else {
-    //     desktopButtons.forEach((button) => {
-    //         button.style.fontSize = '1.25em';
-    //         button.style.opacity = '1';
-    //         button.style.visibility = 'visible';
-    //     });
-    // }
     fullscreenButton.style.fontSize = '1.25em';
     fullscreenButton.style.opacity = '1';
     fullscreenButton.style.visibility = 'visible';
